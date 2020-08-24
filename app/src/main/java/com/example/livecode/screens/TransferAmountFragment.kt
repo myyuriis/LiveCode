c3d950ca58b529b301b14a92830836f14519408f
Muhammad Yusril Yuriis <61834564+myyuriis@users.noreply.github.com>
2020-08-24 16:51:30 +0700

    nav

--- app/src/main/java/com/example/livecode/screens/TransferAmountFragment.kt
+++ app/src/main/java/com/example/livecode/screens/TransferAmountFragment.kt
@@ -5,29 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.livecode.R
+import com.example.livecode.databinding.FragmentTransferAmountBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TransferAmountFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TransferAmountFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +21,12 @@ class TransferAmountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transfer_amount, container, false)
+        val binding: FragmentTransferAmountBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_transfer_amount, container, false)
+        binding.nextTransferAmountButton.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_transferAmountFragment_to_transferSuccessFragment)
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TransferAmountFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TransferAmountFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
